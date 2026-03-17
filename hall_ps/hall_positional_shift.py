07a695c7c09b107fe093d2997e3d140d4f6cb255
Stepan Tsirkin <stepan.tsirkin@epfl.ch>
2026-03-17 02:45:53 +0100

    updated implementation

--- hall_ps/hall_positional_shift.py
+++ hall_ps/hall_positional_shift.py
@@ -2,7 +2,7 @@
 
 from wannierberri.calculators.static import StaticCalculator
 from wannierberri.formula import Formula_ln
-from wannierberri.formula.covariant import DerDcov
+from wannierberri.formula.covariant_basic import Symmetric, tildeFab_d, tildeFab
 from wannierberri.symmetry.point_symmetry import transform_ident, transform_odd
 from wannierberri.utility import cached_einsum, alpha_A, beta_A
 import numpy as np
@@ -13,52 +13,26 @@ bohr = physical_constants['Bohr radius'][0] / angstrom
 eV_au = physical_constants['electron volt-hartree relationship'][0]
 Ang_SI = angstrom
 
-####################################################
-#    Spatially-dispersive conductivity tensor      #
-####################################################
-
-# To keep e^2/hbar as the global factor of SDCT
-
-electron_g_factor = physical_constants['electron g factor'][0]
-# chack if it is correct
-spin_prefactor = electron_g_factor * hbar**2 / \
-    (elementary_charge * electron_mass * angstrom**2)
 
 
-class DerMetricTerm(Formula_ln):
-    """
-    formula for epsilon_bcd D_c g_da
-    """
+class QuantumMetric_ab(Symmetric):
+    
+    def __init__(self, data_K, **parameters):
+        super().__init__(tildeFab, data_K, axes=[0,1],**parameters)
+        self.transformTR = transform_ident
+        self.transformInv = transform_ident
 
-    def __init__(self, data_k, **parameters):
-        super().__init__(data_k, **parameters)
-        self.D = data_k.Dcov
-        self.dD = DerDcov(data_k)
-        if self.external_terms:
-            self.A = data_k.covariant('AA')
-            self.dA = data_k.covariant('AA', gender=1)
+class DerQuantumMetric_ab_d(Symmetric):
 
-    def nn(self, ik, inn, out):
-        Aln = 1j * self.D.ln(ik, inn, out)
-        dAln = 1j * self.dD.ln(ik, inn, out)
-        Anl = 1j * self.D.nl(ik, inn, out)
-        dAnl = 1j * self.dD.nl(ik, inn, out)
-        if self.external_terms:
-            Aln += self.A.ln(ik, inn, out)
-            dAln += self.dA.ln(ik, inn, out)
-            Anl += self.A.nl(ik, inn, out)
-            dAnl += self.dA.nl(ik, inn, out)
-        term1 = cached_einsum("mla,lndc->mncda", Anl, dAln)
-        term2 = cached_einsum("mlac,lnd->mncda", dAnl, Aln)
-        res = term1 + term2
-        res = 0.5*(res[:, :, alpha_A, beta_A] - res[:, :, beta_A, alpha_A])
-        return res
-
-    def ln(self, ik, inn, out):
-        raise NotImplementedError("ln not implemented for DerMetricTerm yet")
+    def __init__(self, data_K, **parameters):
+        super().__init__(tildeFab_d, data_K, axes=[0,1], **parameters)
+        self.transformTR = transform_odd
+        self.transformInv = transform_odd
 
 
 class PositionalShiftFormula(Formula_ln):
+
+    from wannierberri.data_K.sdct_K import m_spin_prefactor
 
     def __init__(self, data_k,
                  spin_part=False,
@@ -71,18 +45,14 @@ class PositionalShiftFormula(Formula_ln):
         self.spin_part = spin_part
         self.morb_part = morb_part
         self.metric_part = metric_part
-        if self.external_terms:
+        if self.morb_part or self.spin_part:
+            self.A = data_k.SDCT.get_E1(external_terms=self.external_terms)
             if self.morb_part:
-                self.M = data_k.SDCT.M1
-            self.A = data_k.A_H
-        else:
-            if self.morb_part:
-                self.M = data_k.SDCT.M1_internal
-            self.A = data_k.A_H_internal
-        if self.spin_part:
-            self.S = data_k.covariant('SS')
+                self.M = data_k.SDCT.get_M1(external_terms=self.external_terms)
+            if self.spin_part:
+                self.S = data_k.covariant('SS') 
         if self.metric_part:
-            self.DerMetric = DerMetricTerm(
+            self.DerMetric = DerQuantumMetric_ab_d(
                 data_k, external_terms=self.external_terms)
         self.dEig_inv = data_k.dEig_inv
 
@@ -94,15 +64,12 @@ class PositionalShiftFormula(Formula_ln):
             if self.morb_part:
                 M += self.M[ik, out][:, inn]
             if self.spin_part:
-                M += self.S.ln(ik, inn, out) * spin_prefactor
+                M += self.S.ln(ik, inn, out) * m_spin_prefactor
             res[rng, rng, :, :] += 2*cached_einsum("nma,mnb,nm->nab",
-                                                   self.A[ik, inn][:, out],
-                                                   M,
-                                                   self.dEig_inv[ik,
-                                                                 inn][:, out],
-                                                   ).real
+                                    self.A[ik, inn][:, out], M, self.dEig_inv[ik, inn][:, out] ).real
         if self.metric_part:
-            res += self.DerMetric.nn(ik, inn, out)
+            dGda_c = self.DerMetric.nn(ik, inn, out)
+            res[: , :, :, : ] += 0.5 * (dGda_c[:, :,  :, beta_A, alpha_A] - dGda_c[:, :, :, alpha_A, beta_A])
         return res
 
     def ln(self, ik, inn, out):
@@ -124,15 +91,15 @@ class HallPositionalShiftFormula(Formula_ln):
                                         morb_part=morb_part,
                                         metric_part=metric_part,
                                         **parameters)
-        self.ndim = 1
+        self.ndim = 2
         self.transformTR = transform_odd
         self.transformInv = transform_ident
 
     def nn(self, ik, inn, out):
         v = self.V.nn(ik, inn, out)
         F = self.F.nn(ik, inn, out)
-        vF = cached_einsum("mna,nmbc->abc", v, F)
-        return vF - vF.swapaxes(0, 1)
+        vF = cached_einsum("lma,mnbc->lnabc", v, F)
+        return vF[:,:,alpha_A, beta_A,:] - vF[:,:,beta_A, alpha_A,:]
 
     def ln(self, ik, inn, out):
         raise NotImplementedError(
--- run.py
+++ run.py
@@ -1,26 +1,35 @@
-from hall_positional_shift import HallPositionalShift
-from wannierberri.models import Chiral_OSD
+from hall_ps.hall_positional_shift import HallPositionalShift
+from hall_ps.models import model_TaRh2B2
 from wannierberri.system import System_PythTB
 from wannierberri.grid import Grid
 from wannierberri import run
+from wannierberri.calculators.static import Hall_classic_FermiSurf, AHC_Zeeman_spin, AHC_Zeeman_orb 
+
+
 import numpy as np
-model = Chiral_OSD()
+model = model_TaRh2B2(t1=1, t2=0.5, t3=0.3)
 system = System_PythTB(model, spin=True)
 
 efermi = np.linspace(-2, 2, 41)
 
+tetra = True
 
 calculators = {
     "hall_positional_shift_morb":
-        HallPositionalShift(Efermi=efermi,
-                            kwargs_formula={"morb_part": True}),
-    "hall_positional_shift_spin":
-        HallPositionalShift(Efermi=efermi,
-                            kwargs_formula={"spin_part": True}),
+        HallPositionalShift(Efermi=efermi, tetra=tetra,
+                            kwargs_formula={"morb_part": True},),
+    # "hall_positional_shift_spin":
+    #     HallPositionalShift(Efermi=efermi,
+    #                         kwargs_formula={"spin_part": True, "tetra":tetra},),
 
     "hall_positional_shift_metric":
-        HallPositionalShift(Efermi=efermi,
-                            kwargs_formula={"metric_part": True}),
+        HallPositionalShift(Efermi=efermi, tetra=tetra,
+                            kwargs_formula={"metric_part": True},),
+
+    "hall_classical": HallPositionalShift(Efermi=efermi, tetra=tetra, 
+                                           kwargs_formula={"metric_part": True},),
+
+    "hall_MOmega": AHC_Zeeman_orb(Efermi=efermi, tetra=tetra),
 }
 
 grid = Grid(system=system, length=30, length_FFT=15)
